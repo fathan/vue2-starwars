5b19709f155b3191c28e0b6dbf77413c205f4445
FathanRohman <doctor.fathan@gmail.com>
2017-12-11 18:21:54 +0700

    add new production docs

--- config/index.js
+++ config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'https://fathan.github.io/vue2-starwars/',
 
     /**
      * Source Maps
